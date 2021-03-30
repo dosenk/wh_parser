d36ecc6c164b75515e5bc316fd0a5ad009ad865e
vladislavgribkov <vladislavgrbkovq@yandex.by>
2021-03-30 17:29:35 +0300

    add whatsapp class

--- public/javascripts/socketClient.js
+++ public/javascripts/socketClient.js
@@ -1,8 +1,40 @@
 const SOCKET_SERVER = "http://localhost:3000";
 
+class WhatsApp {
+  constructor() {
+      this.button = document.querySelector('.block-btn');
+      this.addListener();
+  }
+
+  addListener(){
+      this.button.addEventListener('click', ()=>{
+          console.log(this.request('luntik'));
+      });
+  }
+
+  async request(nameObject) {
+      const body = JSON.stringify({
+        name: nameObject
+      });
+
+      const response = await fetch('http://localhost:3000/qr', {
+          method: 'post',
+          headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+          },
+          body,
+        })
+          .then((res) => console.log(res));
+      return response;
+  }
+}
+
 class SocketIoClient {
   constructor() {
     this.socket = io(SOCKET_SERVER);
+    this.whatsApp = new WhatsApp();
+    this.listenSocketEvents();
   }
 
   sendName(name) {
@@ -10,8 +42,11 @@ class SocketIoClient {
   }
 
   listenSocketEvents() {
-    this.socket.on(EVENT_CONNECT, () => {
-      // событи будет сробатывать при подключении к сокету
+    this.socket.on('connect', () => {
+      console.log(1)
+    });
+    this.socket.on('qr', (img) => {
+      console.log('hello');
     });
   }
   sendMessage = (msg) => {
